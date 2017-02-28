363f77a717e21981e389bad4b5c12ef69ab19f51
Evan Engels <evan.engels@gmail.com>
2017-02-27 21:45:41 -0600

    item view of contact list

--- app/view/contact-list.js
+++ app/view/contact-list.js
@@ -1,6 +1,46 @@
+class ItemView {
+  constructor(data, store) {
+    this.data = data;
+    this.store = store;
+
+    this.el = document.createElement('div');
+    this.el.classList.add('grid__item');
+
+    this.el.innerHTML = `
+      <div class="card">
+        <div class="card__info">
+          <h1 class="name"></h1>
+          <p class="info address"></p>
+          <p class="info citystate"></p>
+        </div>
+        <div class="card__button">
+          <button class="button delete">Delete</button>
+        </div>
+      </div>`;
+  }
+
+  render() {
+    this.el.querySelector('.name').innerText =
+  }
+}
+
 export default class ContactListView {
   constructor(el, store) {
     this.el = el;
+    this.store = store;
+  }
 
+  mounted() {
+    this.store.subscribe(() => {
+      this.render();
+    });
+  }
+
+  render() {
+    this.el.innerHTML = '';
+    const contacts = this.store.getState().contacts;
+    contacts.forEach((data) => {
+      const view = new ItemView(data, this.store);
+    });
   }
 }
